05d8487cdce44c202c3ae65042f558a7f6f24920
Nathan Vieira <57924342+Goytai@users.noreply.github.com>
2021-04-26 18:11:00 -0300

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,19 @@ export default class MyDocument extends Document {
         return (
             <Html>
                 <Head>
+                    
+                    <meta property="og:locale" content="pt_BR">
+
+                    <meta property="og:url" content="https://nlw5.vercel.app/">
+
+                    <meta property="og:title" content="O melhor para você ouvir, sempre">
+                    <meta property="og:site_name" content="Podcastr">
+                    
+                    <meta property="og:image" content="https://raw.githubusercontent.com/Goytai/NLW5/01a75a2c4e3a5202dd7c58e35a83c97559855ba3/.github/banner.svg">
+                    <meta property="og:image:type" content="image/jpeg">
+                    <meta property="og:image:width" content="800"> /** PIXELS **/
+                    <meta property="og:image:height" content="600">
+                    
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Inter&family=Lexend:wght@500;600&display=swap" rel="stylesheet" />
 
